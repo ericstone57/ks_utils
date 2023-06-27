43caadf0779f3608bff32a03120c3dc0696876e2
Eric Lee <ericstone.dev@gmail.com>
2023-06-27 16:04:21 +0800

    add pagination to ninja

---
+++ ks_utils/ninja/pagination.py
@@ -0,0 +1,29 @@
+from typing import List, Any
+
+from django.db.models import QuerySet
+from ninja import Schema
+from ninja.conf import settings
+from ninja.pagination import PageNumberPagination
+from pydantic import Field
+
+
+class KSPagination(PageNumberPagination):
+    class Input(Schema):
+        page: int = Field(1, ge=1)
+        per_page: int = Field(settings.PAGINATION_PER_PAGE, ge=1)
+
+    class Output(Schema):
+        items: List[Any]
+        total: int
+
+    def paginate_queryset(
+        self,
+        queryset: QuerySet,
+        pagination: Input,
+        **params: Any,
+    ) -> Any:
+        offset = (pagination.page - 1) * self.page_size
+        return {
+            "items": queryset[offset : offset + self.page_size],
+            "total": self._items_count(queryset),
+        }
